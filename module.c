addb174db6a1ed74db768981fc25f17c64d3b1f7
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-16 11:53:42 +0330

    add: kernel module

---
+++ module.c
@@ -0,0 +1,110 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/skbuff.h>
+#include <linux/netdevice.h>
+#include <linux/netfilter_ipv4/ip_tables.h>
+
+static struct nf_hook_ops nfho;
+
+struct packet_info {
+    struct sk_buff *skb;
+    int pid;
+};
+
+#define MAX_QUEUE_SIZE 1000
+struct packet_info packet_queue[MAX_QUEUE_SIZE];
+int queue_index = 0;
+
+static void insert_packet(struct sk_buff *skb, int pid) {
+    if (queue_index < MAX_QUEUE_SIZE) {
+        packet_queue[queue_index].skb = skb;
+        packet_queue[queue_index].pid = pid;
+        queue_index++;
+    } else {
+        printk(KERN_INFO "Packet queue full, dropping packets.\n");
+    }
+}
+
+static int compare(const void *a, const void *b) {
+    return ((struct packet_info *)a)->pid - ((struct packet_info *)b)->pid;
+}
+
+static void process_queue(void) {
+    if (queue_index > 0) {
+        qsort(packet_queue, queue_index, sizeof(struct packet_info), compare);
+        for (int i = 0; i < queue_index; i++) {
+            struct sk_buff *skb = packet_queue[i].skb;
+            struct iphdr *ip_header;
+            struct tcphdr *tcp_header;
+            unsigned int data_len;
+            unsigned char *data;
+
+            ip_header = ip_hdr(skb);
+
+            if (ip_header->protocol == IPPROTO_TCP) {
+                tcp_header = tcp_hdr(skb);
+                data_len = skb->len - sizeof(struct iphdr) - tcp_header->doff * 4;
+                data = (unsigned char *)(tcp_header) + tcp_header->doff * 4;
+
+                printk(KERN_INFO "Captured Packet - Source IP: %pI4, Destination IP: %pI4, Source Port: %d, Destination Port: %d\n",
+                       &ip_header->saddr, &ip_header->daddr, ntohs(tcp_header->source), ntohs(tcp_header->dest));
+
+                if (data_len > 0) {
+                    printk(KERN_INFO "Data: %.*s\n", data_len, data);
+                }
+            }
+
+            kfree_skb(skb);
+        }
+        queue_index = 0;
+    }
+}
+
+unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
+    struct iphdr *ip_header;
+    struct tcphdr *tcp_header;
+    struct net_device *outdev;
+    int pid = 0;
+
+    if (!skb) {
+        return NF_ACCEPT;
+    }
+
+    ip_header = ip_hdr(skb);
+    outdev = skb->dev;
+
+    if (outdev && outdev->type == ARPHRD_ETHER && ip_header->protocol == IPPROTO_TCP) {
+        pid = task_pid_nr(current);
+        insert_packet(skb, pid);
+    }
+
+    process_queue();
+
+    return NF_ACCEPT;
+}
+
+static int __init packet_sniffer_init(void) {
+    nfho.hook = hook_func;
+    nfho.hooknum = NF_INET_POST_ROUTING;
+    nfho.pf = PF_INET;
+    nfho.priority = NF_IP_PRI_FIRST;
+
+    nf_register_net_hook(&init_net, &nfho);
+    printk(KERN_INFO "Packet sniffer loaded.\n");
+
+    return 0;
+}
+
+static void __exit packet_sniffer_exit(void) {
+    nf_unregister_net_hook(&init_net, &nfho);
+    printk(KERN_INFO "Packet sniffer unloaded.\n");
+}
+
+module_init(packet_sniffer_init);
+module_exit(packet_sniffer_exit);
+
+MODULE_LICENSE("GPL");
